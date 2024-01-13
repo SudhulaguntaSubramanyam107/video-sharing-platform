6bc6a84f1a99dbea07d9f733bd82d9d6f6785de4
subramanyam <sunnycreative19@gmail.com>
2024-01-13 13:03:53 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import App from "./App"
+ 
+
+let ele=document.getElementById("root")
+let root=ReactDOM.createRoot(ele)
+root.render(
+<App>
+</App> 
+
+)
